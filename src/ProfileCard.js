f7a9ba29d8aac9295d6495393f756b868853d83c
benjamin <benbhbe@yahoo.com>
2023-10-24 22:04:44 -0400

    Made minor changes to two files

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,4 +1,10 @@
-function ProfileCard() {
-  return <div>ProfileCard!!!</div>;
+function ProfileCard({ title, handle }) {
+  //const { title, handle } = props;
+  return (
+    <div>
+      <div>Title is {title}</div>
+      <div>Handle is {handle}</div>
+    </div>
+  );
 }
 export default ProfileCard;
